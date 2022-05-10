0b2f4e661a3fa8c93b7458cc685373c565752cde
Christian Duran <sistemas.dmg@ultragasmexico.com>
2022-05-10 16:09:18 -0500

    Google Sign added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcryptjs = require('bcryptjs');
 const { response } = require('express');
+const { json } = require('express/lib/response');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 const Usuario = require('../models/usuario');
 
 
@@ -56,11 +58,80 @@ const login = async (req, res = response) => {
         });
 
     }
+  
     
 
 }
 
 
+const googleSignin = async( req, res = response ) =>{
+
+    const { id_token } = req.body;
+    console.log("entrando a googleSignin");
+
+    try {
+
+        const { correo, nombre, img } = await googleVerify( id_token );
+
+        //validar referencia si es que el correo ya existe en nuestra DB
+        let usuario = await Usuario.findOne({ correo });
+
+        //console.log("printing token de google...", id_token);
+
+
+        if(!usuario){
+            console.log("Creando al usuario...");
+            //se debe crear el usuario si no existe
+            const data = {
+                nombre, 
+                correo,
+                password: 'no valida, es de relleno',
+                img,
+                google: true,
+                rol: 'USER_ROLE'
+                
+            };
+            //console.log("datos de google: ", data);
+
+           usuario = new Usuario( data );
+            await usuario.save();
+            console.log("Usuario creado: ", usuario);
+        }
+
+        //si el usuario en DB está en false, entonces negamos su autenticación
+        if(!usuario.estado){
+            console.log("validando status de usuario");
+            return res.status(401).json({
+                msg: 'usuario banneado'
+            });
+        }
+        
+
+        //generar jwt (ya lo tenemos de arriba)
+        const token = await generarJWT( usuario.id );
+        console.log("jwt generado para ", usuario.id);
+
+
+
+        res.json({
+            msg: 'Ok, si tienes tu id_token de google, pasale',
+            token,
+            usuario
+        });
+    
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: "el token no se pudo verificar"
+        })
+    }
+
+   
+}
+
+
+
 module.exports = {
-    login
+    login,
+    googleSignin
 }
